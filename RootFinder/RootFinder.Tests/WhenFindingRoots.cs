7c09081ab344a3e6f59a5d4e26f331e6b3a8ac16
Paul Burrows <paul_burrows1986@hotmail.co.uk>
2012-03-24 16:24:40 +0000

    The root finder works for 1 dimensional functions, using the Newton method. There's no parsing capability yet.

---
+++ RootFinder/RootFinder.Tests/WhenFindingRoots.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RootFinder.Tests
+{
+    [TestClass]
+    public class WhenFindingRoots
+    {
+        private RtFinder _rootFinder;
+
+        [TestInitialize]
+        public void Setup() 
+        {
+            _rootFinder = new RtFinder();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(RootNotFoundException))]
+        public void Constant_function_does_not_have_a_root()
+        {
+            double root = _rootFinder.FindRoot(x => 1);                        
+        }
+
+        [TestMethod]
+        public void Linear_function_has_a_root()
+        {
+            Func<double, double> f = x => x + 1;
+            
+            double root = _rootFinder.FindRoot(f);           
+            
+            Assert.IsTrue(f(root) < _rootFinder.Accuracy);
+        }
+
+        [TestMethod]
+        public void Root_found_for_quadratic_with_two_roots()
+        {
+            Func<double, double> f = x => Math.Pow(x, 2) - 10;
+
+            double root = _rootFinder.FindRoot(f);
+
+            Assert.IsTrue(f(root) < _rootFinder.Accuracy);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(RootNotFoundException))]
+        public void No_root_found_for_quadratic_with_no_roots()
+        {
+            _rootFinder.FindRoot(x => Math.Pow(x, 2) + 10);          
+        }
+    }
+}
---
+++ RootFinder/RootFinder/RtFinder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RootFinder
+{
+    public class RtFinder
+    {        
+        public double Accuracy { get; set; }
+        public double StepSize { get; set; }
+        public int MaxSteps { get; set; }
+        public double Seed { get; set; }        
+
+        public RtFinder() 
+        {            
+            Accuracy = 0.00001;
+            StepSize = 0.00001;
+            MaxSteps = 100;
+            Seed = 0;
+        }
+        
+        public double FindRoot(Func<double, double> f)
+        {
+            double x = Seed;
+                        
+            for (int i = 0; i < MaxSteps; i++)
+            {
+                double y = f(x);
+                
+                if (y < Accuracy)
+                {
+                    return x;
+                }
+
+                x = x - (y / Grad(f, x));
+            }
+
+            throw new RootNotFoundException();            
+        }
+
+        private double Grad(Func<double, double> f, double x)
+        {
+            double y0 = f(x);
+            double y1 = f(x + StepSize);
+
+            return (y1 - y0) / StepSize;
+        }
+    }
+}
